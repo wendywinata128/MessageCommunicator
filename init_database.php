eaae76f72946bfc7202563417faec640644c11ad
Wendy Winata <70324413+wendywinata128@users.noreply.github.com>
2022-04-23 14:46:30 +0800

    Update init_database.php

--- init_database.php
+++ init_database.php
@@ -1,9 +1,10 @@
 <?php
-
-$host = "localhost";
-$user = "root";
-$pass = "";
-$name = "message_communicator";
-
-$con = mysqli_connect($host, $user, $pass, $name);
+include './database.php'
+  
+$con->query('CREATE TABLE message(
+  id INT(6) AUTO_INCREMENT PRIMARY KEY,
+  message VARCHAR(MAX),
+  key VARCHAR(64),
+  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)')
 
